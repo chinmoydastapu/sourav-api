0eab4c6866ddbe0eb68635c81f6ec2041410c438
Chinmoy Das <chinmoydastapu2@gmail.com>
2023-03-04 17:57:12 +0600

    except sorting see all accuracy spinner

--- Javascript/api.js
+++ Javascript/api.js
@@ -12,7 +12,7 @@ function showData(cards) {
         cardDiv.innerHTML = `
             <div class="col">
                 <div class="card h-100">
-                    <img src="${card.image}" class="card-img-top" alt="...">
+                    <img src="${card.image}" class="card-img-top"  alt="...">
                     <h4>Features</h4>
                     <ol type="1">
                         <li>${card.features[0]}</li>
@@ -56,36 +56,49 @@ function modalData(data) {
                 <h1 class="modal-title fs-5" id="cardModalLabel">Modal title</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <div>
+            <div class="modal-body d-lg-flex justify-content-center align-items-center">
+                <div class="">
                     ${data.description}
-                    <div>
-                        pricing
+                    <div class="d-flex gap-3 justify-content-around">
+                        <div class="border p-3">
+                        ${data.pricing?data.pricing[0].price!=="No cost"?data.pricing[0].price:"Free of cost" : 'Free of Cost'}
+                        </div>
+                        <div class="border p-3">
+                        ${data.pricing?data.pricing[1].price!=="No cost"?data.pricing[1].price:"Free of cost" : 'Free of Cost'}
+                        </div>
+                        <div class="border p-3">
+                        ${data.pricing?data.pricing[2].price!=="No cost"?data.pricing[2].price:"Free of cost" : 'Free of Cost'}
+                        </div>                      
                     </div>
                     <div>
                         <div>
                             Features
                             <ul>
-                                Features
+                                ${"<li>" + data.features[1].feature_name + "</li><li>" + data.features[2].feature_name + "</li><li>" + data.features[3].feature_name + "</li><li>" + data?.features[4]?.feature_name + "</li>"}
                             </ul>
                         </div>
                         <div>
                             Integration
                             <ul>
-                                Integrations
+                                <li>${data?.integrations?.length > 0 ? data.integrations.join('<li>') : 'No data found'}
                             </ul>
                         </div>
                     </div>
                 </div>
 
-                <div>
-                    <img src="${data.image_link[0]}" class="img-fluid">
-                    title
-                    description
+                <div class="position-relative">
+                    <img src="${data.image_link[0]}" class="img-fluid" style="height: 50vh;">
+                    <button class="d-none btn btn-danger position-absolute top-0 end-0">${data.accuracy.score ? data.accuracy.score*100 : ''}% Accuracy</button>
                 </div>
             </div>
         </div>
     `;
+}
+
+function loading(data){
+    if(data){
+
+    }
 }
 
 loadData();
